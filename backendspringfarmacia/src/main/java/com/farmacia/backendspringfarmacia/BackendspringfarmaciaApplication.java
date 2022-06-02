ad3356c292f6b807d1750cc7a05a367e71625236
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-06-02 12:41:21 -0300

    Spring Initializr Project

---
+++ backendspringfarmacia/src/main/java/com/farmacia/backendspringfarmacia/BackendspringfarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.backendspringfarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendspringfarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendspringfarmaciaApplication.class, args);
+	}
+
+}
---
+++ backendspringfarmacia/src/test/java/com/farmacia/backendspringfarmacia/BackendspringfarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.backendspringfarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendspringfarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
