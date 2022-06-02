1da8ed6eb0f11da288cabefdf0cb9e32144fd4ce
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-06-02 15:36:47 -0300

    Models Produto e Categoria finalizadas

---
+++ backendspringfarmacia/src/main/java/com/farmacia/backendspringfarmacia/model/Categoria.java
@@ -0,0 +1,50 @@
+package com.farmacia.backendspringfarmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_categoria;
+	
+	@NotNull
+	private boolean necessitaReceita;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String secaoItem;
+
+	public long getId_categoria() {
+		return id_categoria;
+	}
+
+	public void setId_categoria(long id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+
+	public boolean isNecessitaReceita() {
+		return necessitaReceita;
+	}
+
+	public void setNecessitaReceita(boolean necessitaReceita) {
+		this.necessitaReceita = necessitaReceita;
+	}
+
+	public String getSecaoItem() {
+		return secaoItem;
+	}
+
+	public void setSecaoItem(String secaoItem) {
+		this.secaoItem = secaoItem;
+	}
+		
+}
---
+++ backendspringfarmacia/src/main/java/com/farmacia/backendspringfarmacia/model/Produto.java
@@ -0,0 +1,72 @@
+package com.farmacia.backendspringfarmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String nomeProduto;
+	
+	@NotNull
+	private int preco;
+	
+	@Size(min = 5, max = 100)
+	private String laboratorio;
+	
+	@NotNull
+	private int quantidadeEstoque;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public int getPreco() {
+		return preco;
+	}
+
+	public void setPreco(int preco) {
+		this.preco = preco;
+	}
+
+	public String getLaboratorio() {
+		return laboratorio;
+	}
+
+	public void setLaboratorio(String laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public int getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(int quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+}
